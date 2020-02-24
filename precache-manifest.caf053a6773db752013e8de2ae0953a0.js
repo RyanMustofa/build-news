a566b4400f16910000fa5bd10b98d6455bda2c87
RyanMustofa <rm281003@gmail.com>
2020-02-24 08:35:42 +0700

    first commit

---
+++ precache-manifest.caf053a6773db752013e8de2ae0953a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63889ff074f02dc598bd8e6b4aa94953",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f06ddbe6cfd82a02db9",
+    "url": "/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "b51b71013eea153cc63a",
+    "url": "/static/css/main.12317340.chunk.css"
+  },
+  {
+    "revision": "5f06ddbe6cfd82a02db9",
+    "url": "/static/js/2.b99b46eb.chunk.js"
+  },
+  {
+    "revision": "daedb6e9c1e75eb2181f2dde37df55f3",
+    "url": "/static/js/2.b99b46eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b51b71013eea153cc63a",
+    "url": "/static/js/main.5d47c2f2.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
